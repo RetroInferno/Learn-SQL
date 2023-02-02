24acff091004d34786d2643379cb4cd1503cf38e
Zhaowei Ding <86904225+RetroInferno@users.noreply.github.com>
2023-02-01 23:14:12 -0500

    completed up to 10

--- solved.sql
+++ solved.sql
@@ -42,3 +42,37 @@ GROUP BY bands.id
 HAVING COUNT(albums.id) < 1;
 
 -- 6.
+SELECT albums.name AS "Name",
+    albums.release_year AS "Release Year",
+    SUM(songs.length) AS "Duration" FROM
+albums JOIN songs on albums.id = songs.album_id
+GROUP BY albums.id --or song.album_id, since this is a 1:1 relationship
+ORDER BY Duration DESC
+LIMIT 1;
+
+-- 7.
+UPDATE albums SET release_year = 1986
+WHERE release_year is NULL;
+
+UPDATE albums
+SET release_year = 1986
+WHERE id = 4; --SELECT * FROM albums where release_year IS NULL;
+
+-- 8.
+INSERT INTO bands (name)
+VALUES("Bruno Mars");
+SELECT * FROM bands WHERE name = "Bruno Mars";
+
+INSERT INTO albums (name, release_year, band_id)
+VALUES("24K Magic", 2016, 8); -- No way around this usesage of "id" here!
+
+SELECT * FROM
+albums JOIN bands ON albums.band_id = bands.id
+WHERE bands.name = "Bruno Mars";
+
+-- 9.
+DELETE FROM albums WHERE albums.name = "24K Magic"; -- or id = ?
+DELETE FROM bands WHERE name = "Bruno Mars"; -- or id = ?
+
+-- 10.
+SELECT AVG(length) as "Average Song Duration" FROM songs;
